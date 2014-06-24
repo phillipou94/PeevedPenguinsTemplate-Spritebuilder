cdc16b67b5bf749e1ec2b93baf52905a9b767e8e
Phillip Ou <phillipou@Phillips-MacBook-Pro.local>
2014-06-24 12:51:43 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,19 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Phillip Ou on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+-(id) init{
+    self=[super init];
+    if (self){
+        CCLog(@"Penguin Created");
+    }
+    return self;
+}
+@end
---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,19 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Phillip Ou on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+-(id) init{
+    self = [super init];
+    if(self){
+        CCLog(@"Seal Created");
+    }
+    return self;
+}
+@end
