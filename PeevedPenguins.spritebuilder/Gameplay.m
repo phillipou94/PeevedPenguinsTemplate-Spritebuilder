50e301d899a822c440a90bc3a667c0bdba03ba12
Phillip Ou <phillipou@Phillips-MacBook-Pro.local>
2014-06-25 14:01:48 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -161,7 +161,7 @@
     [_contentNode runAction:actionMoveTo];
 }
 
- static const float MIN_SPEED=5.f;
+ static const float MIN_SPEED=1.f;
 
 -(void) update: (CCTime) delta{
     //if speed is below minimum speed, assume attempt is over
