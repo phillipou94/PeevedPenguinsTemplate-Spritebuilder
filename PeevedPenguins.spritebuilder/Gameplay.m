89f07c10db8b4af19bc754e7ff7e9b31ca257c61
Phillip Ou <phillipou@Phillips-MacBook-Pro.local>
2014-06-25 09:10:42 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -28,6 +28,7 @@
 
 -(void) launchPenguins{
     CCNode *penguin = [CCBReader load: @"Penguin"]; //create variable penguin from spriteBuilder file Penguin
+    CCNode *contentNode = [CCBReader load: @"Gameplay"];
     
     //position penguin at bowl of catapult
     penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
@@ -43,7 +44,7 @@
     //ensure object is followed by camera
     self.position = ccp(0,0);
     CCAction *follow= [CCActionFollow actionWithTarget:penguin worldBoundary: self.boundingBox];
-    [self runAction: follow];   //runaction is method that asks camera to follow object
+    [contentNode runAction: follow];   //runaction is method that asks camera to follow object
     
 }
 
